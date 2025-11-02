aa2dd9332540556639ad113b9830bc1313eb0b47
rebka <rebkaa22@gmail.com>
2025-11-02 21:48:04 +0300

    updated url

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,3 +1,24 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   getAnswer,
+//   postAnswer,
+//   deleteAnswer,
+// } = require("../controllers/answerController.js");
+
+// const authMiddleware = require("../middleware/authMiddleware.js");
+
+// // Get answers for a question: GET /api/v1/answer/:questionid
+// router.get("/answer/:questionid", getAnswer);
+
+// // Post a new answer: POST /api/v1/answer
+// router.post("/answer", authMiddleware, postAnswer);
+
+// // Delete an answer: DELETE /api/v1/answer/:answerid
+// router.delete("/answer/:answerid", authMiddleware, deleteAnswer);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
